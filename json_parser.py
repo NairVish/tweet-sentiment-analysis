e50573b47aaffedbe844c17e4b3576c54683a57d
Vishnu Nair <vish@vnair.me>
2018-10-27 10:31:33 -0400

    Commenting JSON parser.

--- json_parser.py
+++ json_parser.py
@@ -1,5 +1,5 @@
 """
-This class cleans json data for tweet sentiment analysis.
+This class cleans JSON data for tweet sentiment analysis.
 Things that it parses for are: links, "\n", "\\", and "\"" in addition to removing duplicates.
 
 Things to do: Remove unicode.
@@ -12,22 +12,48 @@ import os
 
 
 class CleanData:
+    """
+    The data cleaner.
+    """
     def __init__(self, json_file, output_json_file):
+        """
+        Initializes the class.
+        :param json_file: The input tweet data.
+        :param output_json_file: The output file to store the cleaned tweet data.
+        """
         self.json_file = json_file
         self.output_json_file = output_json_file
 
-    def open_json(self, json_file):
+    @staticmethod
+    def open_json(json_file):
+        """
+        Opens the JSON file.
+        :param json_file: The JSON file to open.
+        :return: The loaded data.
+        """
         with open(json_file) as json_data_string:
             ds = json.load(json_data_string)
             print('\t\tThe original size of ' + str(json_file) + ' is: \t' + str(len(ds)))
             return ds
 
-    def write_json(self, json_file, data_to_dump):
+    @staticmethod
+    def write_json(json_file, data_to_dump):
+        """
+        Writes data to the specified JSON file.
+        :param json_file: The
+        :param data_to_dump:
+        :return: The name of the input JSON file.
+        """
         with open(json_file, 'w') as f:
             json.dump(data_to_dump, f)
             return json_file
 
     def remove(self, duplicate):
+        """
+        Cleans the data.
+        :param duplicate: The input JSON data.
+        :return: The cleaned data.
+        """
         final_list = []
         text = []
         pattern_to_eliminate = r"http\S+"
@@ -41,10 +67,13 @@ class CleanData:
         for sentence in list_four:
             clean = re.sub(pattern_to_eliminate, "", sentence)
             text.append(clean)
-        print('\t\tAfter cleaning the size of the file is \t' + str(len(text)))
+        print('\t\tAfter cleaning, the size of the file is \t' + str(len(text)))
         return text
 
     def clean_it(self):
+        """
+        Calls the cleaning procedure.
+        """
         file = self.open_json(self.json_file)
         file = self.remove(file)
         output_file = self.write_json(self.output_json_file, file)
