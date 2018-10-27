816294496f9718b6f2d908622c7c41d5b54099ee
Vishnu Nair <vish@vnair.me>
2018-10-27 09:37:35 -0400

    Reformatting and commenting main script.

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
-from json_parser import CleanData
-from analyze_data import SentimentAnalyzer
-from grab_data import TwitterData
+"""
+The main script.
+
+Takes in a query with the "-q" or "--query" argument.
+Grabs tweets, cleans them, processes them, and generates sentiment bubble map.
+"""
 import argparse
 import os
-import glob
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-q', '--query',
-                    help='Type whatever you are looking for. This can be an @ mention, a hashtag, or any other term.',
-                    type=str, required=True)
-args = parser.parse_args()
+from analyze_data import SentimentAnalyzer
+from grab_data import TwitterData
+from json_parser import CleanData
 
-original_directory = os.getcwd()
-if not os.path.exists(os.path.join(original_directory, "cities.csv")):
-    print("The cities list does not exist. Run shp_to_csv.py to generate it.")
-    os._exit(1)
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q', '--query',
+                        help='Type whatever you are looking for. This can be an @ mention, a hashtag, or any other term.',
+                        type=str, required=True)
+    args = parser.parse_args()
 
-print('Gathering data')
-data = TwitterData('cities.csv', args.query)
-data.gather_data()
+    # check if the cities.csv file exists
+    original_directory = os.getcwd()
+    if not os.path.exists(os.path.join(original_directory, "cities.csv")):
+        print("The cities list does not exist. Run shp_to_csv.py to generate it.")
+        os._exit(1)
 
-print('Cleaning data')
-tweet_directory = os.path.join(os.getcwd(), 'tweets')
-os.chdir(tweet_directory)
+    # get tweets from each city for query
+    print('Gathering data.')
+    data = TwitterData('cities.csv', args.query)
+    data.gather_data()
 
-for file in os.listdir(os.getcwd()):
-    if not file.endswith(".json") or file.startswith("output-"): continue
-    print("\tCleaning {}".format(file))
-    x = CleanData(file, 'output-' + file)
-    x.clean_it()
+    # clean grabbed tweets
+    print('Cleaning data.')
+    tweet_directory = os.path.join(os.getcwd(), 'tweets')
+    os.chdir(tweet_directory)
 
-os.chdir(original_directory)
-print('Starting sentiment analyzer and plotter')
+    for file in os.listdir(os.getcwd()):
+        if not file.endswith(".json") or file.startswith("output-"): continue
+        print("\tCleaning {}".format(file))
+        x = CleanData(file, 'output-' + file)
+        x.clean_it()
 
-x = SentimentAnalyzer('cities.csv')
-x.run_it()
+    os.chdir(original_directory)
+
+    # plot sentiments
+    print('Starting sentiment analyzer and plotter.')
+    x = SentimentAnalyzer('cities.csv')
+    x.run_it()
